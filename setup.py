1136abd4d41a8dd556f7c966ec90e959a9a86f8d
Michal Horejsek <horejsekmichal@gmail.com>
2011-07-14 17:45:40 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-version = '1.4'
+version = '1.5'
 
 setup(
     name = 'colorbash',
@@ -14,12 +14,15 @@ setup(
     author_email = 'horejsekmichal@gmail.com',
     license = 'GNU General Public License (GPL)',
     classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
         'Development Status :: 5 - Production/Stable',
-         'License :: OSI Approved :: GNU General Public License (GPL)',
-         'Operating System :: OS Independent',
-         'Programming Language :: Python',
-         'Programming Language :: Python :: 2',
-         'Topic :: Software Development :: Libraries :: Python Modules',
-         'Topic :: Text Processing :: Filters',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Terminals',
     ],
 )
